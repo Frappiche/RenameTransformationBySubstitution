9ff48e4e35d98062392d4b772a5347e9843694b6
Frappiche <marionfrappe@orange.fr>
2025-09-12 16:26:57 +0200

    test_pas_concluants 12/09

--- RenameTransformationBySubstitution.js
+++ RenameTransformationBySubstitution.js
@@ -14,28 +14,29 @@ function RenameTransformationBySubstitution() {
 
     // Key.String.Drawing.name(int elementld, int drawingIndex) leementld serai l'id unique de l'élément ?
 
-    var SelectedRead =selection.selectedNode(0)
+    var SelectedRead = selection.selectedNode(0)
     var SelectedReadName = node.getName(SelectedRead)
     var CurrentFrame = Timeline.firstFrameSel
 
-    //MessageLog.trace("Tu as sélectionné " + SelectedReadName + " à la frame "+ CurrentFrame)
+    MessageLog.trace("Tu as sélectionné " + SelectedReadName + " à la frame " + CurrentFrame)
 
-    var CurrentSubName = FSH_get_exposed_sub(SelectedRead,CurrentFrame)
+    var CurrentSubName = FSH_get_exposed_sub(SelectedRead, CurrentFrame)
     //MessageLog.trace("La substitution actuelle est "+ CurrentSub)
-    
+
 
     //get the transformation switch attributes
-    var DefGroup = get_Deformation_path(SelectedRead) 
+    var DefGroup = get_Deformation_path(SelectedRead)
 
     TransSwitch = DefGroup + "/Transformation-Switch"
     MessageLog.trace(TransSwitch)
 
+    MessageLog.trace(outputTransformationNames(TransSwitch))
 
+    /*
     var TransSwitchAttr = FSH_show_attributes(TransSwitch,CurrentFrame)
 
     MessageLog.trace(MessageLog.trace(TransSwitchAttr))
 
-/*
 
     SetCurrentTransformationName(CurrentSubName)
 
@@ -53,55 +54,68 @@ function get_Deformation_path(_node) {
 }
 
 
-function FSH_get_exposed_sub(_node,_frame){
-    
+function FSH_get_exposed_sub(_node, _frame) {
+
     //MessageLog.trace("FSH_get_exposed_sub ("+_node+") ("+_frame+")")
     // return all the name of the exposed sub at a frame
     const aframe = _frame != undefined ? _frame : frame.current()
-    if(node.type(_node)!="READ"){
+    if (node.type(_node) != "READ") {
         //MessageLog.trace("Error node "+_node+" is not a READ ")
-        return 
+        return
     }
     var previously_selected_nodes = selection.selectedNodes()
     selection.clearSelection()
     selection.addNodeToSelection(_node)
     var readcol = Timeline.selToColumn(0);
-    var sub_name = column.getEntry(readcol,1,aframe);
+    var sub_name = column.getEntry(readcol, 1, aframe);
     selection.clearSelection()
     selection.addNodesToSelection(previously_selected_nodes)
     //MessageLog.trace("return ("+sub_name+")")
     return sub_name
 }
 
-    
-function FSH_show_attributes(_node,_frame){
+
+function FSH_show_attributes(_node, _frame) {
     const aframe = _frame != undefined ? _frame : frame.current()
     var table = []
     var names = []
-    attributes = node.getAttrList(_node,_frame)
-    for(var a in attributes){
+    attributes = node.getAttrList(_node, _frame)
+    for (var a in attributes) {
         const attr = attributes[a]
         names.push(attr.fullKeyword())
 
-        if(attr.hasSubAttributes()){
+        if (attr.hasSubAttributes()) {
             sub_attr = attr.getSubAttributes()
-            for (var b in sub_attr){
+            for (var b in sub_attr) {
                 var sub = sub_attr[b]
                 names.push(sub.fullKeyword())
             }
         }
-        
+
     }
-    var msg = "\n-------------------ATTRIBUTES OF "+_node+" ( "+node.type(_node)+") ----------------\n"
+    var msg = "\n-------------------ATTRIBUTES OF " + _node + " ( " + node.type(_node) + ") ----------------\n"
     names.sort()
-    for(var n in names){
+    for (var n in names) {
         const name = names[n]
-        const value = node.getTextAttr(_node,aframe,name);
+        const value = node.getTextAttr(_node, aframe, name);
         table[name] = value
-        msg+="\n"+name+" : ("+value+") type["+typeof(value)+"]"
+        msg += "\n" + name + " : (" + value + ") type[" + typeof (value) + "]"
     }
-    msg+="\n--------------------------------------------------------------------------\n"
+    msg += "\n--------------------------------------------------------------------------\n"
     MessageLog.trace(msg)
     //MessageBox.information(msg)
     return table
 }
+
+function outputTransformationNames(_trSwitch) {
+    var transformationSwitchNodes = _trSwitch
+    var transformationNamesAttribute = node.getAttr(transformationSwitchNodes[0], 1, "TransformationNames");
+
+    if (transformationNamesAttribute) {
+        var subAttributes = transformationNamesAttribute.getSubAttributes();
+        for (var i = 0; i < subAttributes.length; i++) {
+            MessageLog.trace("subAttribute " + i + ", name: " + subAttributes[i].name() + ", value: " + subAttributes[i].textValue());
+        }
+    }
+}
+
